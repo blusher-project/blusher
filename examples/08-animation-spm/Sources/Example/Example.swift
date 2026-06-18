4b28201400f14bfe09509481060030070489dc65
Aspen Schneider <rendezvous71@outlook.com>
2026-06-18 09:02:34 +0900

    Correct popup position in the example.

--- examples/08-animation-spm/Sources/Example/Example.swift
+++ examples/08-animation-spm/Sources/Example/Example.swift
@@ -91,12 +91,12 @@ public struct Program {
             parent: view,
             "Start"
         )
+        toggleButton.geometry = Rect(x: 100.0, y: 250.0, width: 60.0, height: 30.0)
 
         let warning = BPopup(at: toggleButton.absolutePosition, window)
         warning.size = SizeI(width: 80, height: 30)
         warning.rootViewColor = Color(r: 0.0, g: 0.0, b: 0.0, a: 1.0)
 
-        toggleButton.geometry = Rect(x: 100.0, y: 250.0, width: 60.0, height: 30.0)
         toggleButton.onPointerClick += { event in
             if !isRunning {
                 linear.startAnimation()
